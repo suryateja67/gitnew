011b35e1efc90ed974c8026d4bffc8e137284ca8
Suryateja <dodlasuryateja67@gmail.com>
2023-08-10 01:52:27 +0530

    To create a form which reverses back and shows the tasks and also changed some syntaxes to optimize the code

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django import forms
-tasks=["foo","bar","baz"]
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+tasks=[]
 
 class NewTaskform(forms.Form):
     task=forms.CharField(label="New Task")
     priority=forms.IntegerField(label="Priority",min_value=1,max_value=10)
 # Create your views here.
 def index(request):
-    return render(request,"tasks/index.html",{"tasks":tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+    return render(request,"tasks/index.html",{"tasks": request.session["tasks"]})
 def add(request):
+    if request.method=="POST":
+        form=NewTaskform(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"]+=[task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html",{"form":form})
     return render(request, "tasks/add.html",{"form":NewTaskform()})
